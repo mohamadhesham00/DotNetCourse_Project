4de10a8779a245b6cfdb8ef02b5a4958d998e0ac
mohamadhesham00 <mohamadhesham112@gmail.com>
2023-08-07 18:02:15 +0300

    Category created

---
+++ DotNetCourse/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using DotNetCourse.Data;
+using DotNetCourse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCourse.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> ObjCategoryList = _db.Categories.ToList();
+            return View(ObjCategoryList);
+        }
+    }
+}
