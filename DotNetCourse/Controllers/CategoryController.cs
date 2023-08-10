21d11abca53dbbc6c70534a68de7bcd2c3aa7871
mohamadhesham00 <mohamadhesham112@gmail.com>
2023-08-10 19:37:19 +0300

    Added CRUD operations on category

--- DotNetCourse/Controllers/CategoryController.cs
+++ DotNetCourse/Controllers/CategoryController.cs
@@ -16,5 +16,73 @@ namespace DotNetCourse.Controllers
             List<Category> ObjCategoryList = _db.Categories.ToList();
             return View(ObjCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "category created successfully";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null||id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryfromdb = _db.Categories.Find(id);
+            if(categoryfromdb == null) {
+                return NotFound();
+            }
+            return View(categoryfromdb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "category edited successfully";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+        public IActionResult Delete (int? id)
+        {
+            if(id == null||id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryfromdb = _db.Categories.Find(id);
+            if(categoryfromdb == null) {
+                return NotFound();
+            }
+            return View(categoryfromdb);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeleteCategory(int? id)
+        {
+            Category categoryfromdb= _db.Categories.Find(id);
+            if(categoryfromdb == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(categoryfromdb);
+            _db.SaveChanges();
+            TempData["success"] = "category deleted successfully";
+            return RedirectToAction("Index", "Category");
+        }
     }
 }
--- DotNetCourse/Models/Category.cs
+++ DotNetCourse/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DotNetCourse.Models
 {
@@ -6,8 +7,12 @@ namespace DotNetCourse.Models
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("Category Name")]
         [Required]
         public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1,100)]
         public int DisplayOrder { get; set; }
     }
 }
